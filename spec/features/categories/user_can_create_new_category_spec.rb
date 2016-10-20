4aff72bfb44ec26eb6d1e5882fda16dc83bbb438
Ben Pepper <bfpepper87@gmail.com>
2016-10-20 14:47:08 -0600

    add create functionality and tests.

--- spec/features/categories/user_can_create_new_category_spec.rb
+++ spec/features/categories/user_can_create_new_category_spec.rb
@@ -3,9 +3,12 @@ require 'rails_helper'
 RSpec.feature "USer can create a new cateegory" do
   scenario "USer can create a new category" do
 
-    category = Category.create(title: "Cars")
+    visit new_category_path
 
-    visit 
+    fill_in 'category[title]', with: "Cars"
+    click_on "Create Category"
+
+    expect(page).to have_content("Cars")
 
   end
 end
