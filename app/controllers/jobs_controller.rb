509ac4c472be9d4ca61c3d335891f818c85d4308
Ben Pepper <bfpepper87@gmail.com>
2016-10-21 13:09:49 -0600

    add contact view

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,6 +4,7 @@ class JobsController < ApplicationController
 
   def index
     @jobs = @company.jobs
+    @contact = Contact.new
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
     resources :jobs
   end
 
-  resources :comments, only: [:new, :create]
+  resources :comments, only: [:create]
 
   resources :categories
+
+  resources :contacts, only: [:create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
